83bac86402fcc45fa76a43238cf6e4b7570c9ef8
Yiğit Kağan Akça <56603023+ykakca@users.noreply.github.com>
2024-01-24 18:07:48 +0300

    Update web.py

--- web.py
+++ web.py
@@ -6,6 +6,8 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import streamlit as st
 
+@st.cache(ttl=24*3600)
+
 st.title("BERT İle Türkçe Soru Cevaplama Sistemi")
 st.divider()
 st.caption("Yıldız Teknik Üniversitesi")
